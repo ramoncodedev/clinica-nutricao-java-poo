d2accc17922b574ddd316bb9ec0bde02beddee8f
ramoncodedev <ramonwebdesigner2025@gmail.com>
2026-01-05 22:51:08 -0300

    modificando a pasta ealthApplication,que não tava declarada

--- demo/src/main/java/com/clinicahealth/demo/ealthApplication.java
+++ demo/src/main/java/com/clinicahealth/demo/ealthApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ealthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HealthApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(ealthApplication.class, args);}
 
 }
